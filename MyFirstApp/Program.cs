7efe56fc6d7d32ca9d69fe0751a0177400b93c15
ann 🌼 <annleenk@gmail.com>
2024-01-07 19:07:33 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment 
+
+//rakendus mis küsib kasutajalt nime 
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+
+string userName;
+
+userName = Console.ReadLine();
+
+//Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
